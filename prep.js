26d3057b91e577c3c0e7d01fb6ada4835f652e10
miguelemmuel-sudo <miguelemmuel@gmail.com>
2026-08-13 13:35:45 +0200

    Chore: Remove temp files

--- prep.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let rls = fs.readFileSync('update_rls.sql', 'utf8');
-let auth = fs.readFileSync('setup_auth.sql', 'utf8');
-let rbac = fs.readFileSync('rbac_functions.sql', 'utf8');
-let query = rls + '\n' + auth + '\n' + rbac;
-fs.writeFileSync('query.json', JSON.stringify({ project_id: 'ouraqvirmashzzstkqfx', query: query }));
